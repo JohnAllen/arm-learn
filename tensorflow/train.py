59c00e314d7ce79ae13b1b2639833fa01ed87a4c
JohnAllen <johnjalleniii@gmail.com>
2016-05-04 08:47:52 -0700

    add test images and further TF draft

--- tensorflow/train.py
+++ tensorflow/train.py
@@ -6,6 +6,17 @@ import os
 import numpy
 import tensorflow as tf
 
+# Parameters
+learning_rate = 0.001
+training_iters = 1000
+batch_size = 1 
+display_step = 1
+
+n_input = 784
+n_classes = 8 
+dropout = 0.8 # Dropout, probability to keep units
+keep_prob = tf.placeholder(tf.float32) # dropout (keep probability)
+
 
 def read_my_file_format(filename_and_label_tensor):
   """Consumes a single filename and label as a ' '-delimited string.
@@ -13,35 +24,117 @@ def read_my_file_format(filename_and_label_tensor):
     filename_and_label_tensor: A scalar string tensor.
 
   Returns:
-    Two tensors: the decoded image, and the string label.
+Two tensors: the decoded image, and the string label.
   """
   
   record_defaults = [tf.constant([], dtype=tf.string), 
-                   tf.constant([], dtype=tf.float32),   
                    tf.constant([], dtype=tf.float32)]  
  
-  filename, servo_0, servo_1 = tf.decode_csv(filename_and_label_tensor, record_defaults, " ")
+  filename, label = tf.decode_csv(filename_and_label_tensor, record_defaults, " ")
   file_contents = tf.read_file(filename)
   example = tf.image.decode_jpeg(file_contents, 1)
-  return example, servo_0, servo_1
+  return example, label
 
-images = tf.train.match_filenames_once("../images/*.JPG")
-image_queue = tf.train.string_input_producer(images, num_epochs=None, shuffle=True, seed=None, capacity=32, shared_name=None, name=None)
+def conv2d(name, l_input, w, b):
+    return tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(l_input, w, strides=[1, 1, 1, 1], padding='SAME'),b), name=name)
 
-IMAGE_SIZE = 150
-IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE
-batch_size = 1
+def max_pool(name, l_input, k):
+    return tf.nn.max_pool(l_input, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME', name=name)
 
-f = open("paths_labels.txt")
+def norm(name, l_input, lsize=4):
+    return tf.nn.lrn(l_input, lsize, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name=name)
+
+def alex_net(_X, _weights, _biases, _dropout):
+    # Reshape input picture
+    _X = tf.reshape(_X, shape=[-1, 28, 28, 1])
+
+    # Convolution Layer
+    conv1 = conv2d('conv1', _X, _weights['wc1'], _biases['bc1'])
+    # Max Pooling (down-sampling)
+    pool1 = max_pool('pool1', conv1, k=2)
+    # Apply Normalization
+    norm1 = norm('norm1', pool1, lsize=4)
+    # Apply Dropout
+    norm1 = tf.nn.dropout(norm1, _dropout)
+
+    # Convolution Layer
+    conv2 = conv2d('conv2', norm1, _weights['wc2'], _biases['bc2'])
+    # Max Pooling (down-sampling)
+    pool2 = max_pool('pool2', conv2, k=2)
+    # Apply Normalization
+    norm2 = norm('norm2', pool2, lsize=4)
+    # Apply Dropout
+    norm2 = tf.nn.dropout(norm2, _dropout)
+
+        # Fully connected layer
+    dense1 = tf.reshape(norm2, [-1, _weights['wd1'].get_shape().as_list()[0]]) # Reshape conv3 output to fit dense layer input
+    dense1 = tf.nn.relu(tf.matmul(dense1, _weights['wd1']) + _biases['bd1'], name='fc1') # Relu activation
+
+    dense2 = tf.nn.relu(tf.matmul(dense1, _weights['wd2']) + _biases['bd2'], name='fc2') # Relu activation
+
+    # Output, class prediction
+    out = tf.matmul(dense2, _weights['out']) + _biases['out']
+    return out
+
+
+image_files = tf.train.match_filenames_once("./*JPG")
+image_queue = tf.train.string_input_producer(image_files, num_epochs=None, shuffle=True, seed=None, capacity=32, shared_name=None, name=None)
+
+f = open("28_paths.txt")
 file_lines = f.readlines()
 file_queue = tf.train.string_input_producer(file_lines) 
-image, servo_0, servo_1 = read_my_file_format(file_queue.dequeue())
+images, labels = read_my_file_format(file_queue.dequeue())
 
-x = tf.placeholder(tf.float32, shape=(batch_size, IMAGE_SIZE))
-y = tf.placeholder(tf.float32, shape=(batch_size))
-
+# tf Graph input
+x = tf.placeholder(tf.float32, [None, n_input])
+y = tf.placeholder(tf.float32, [None, n_classes])
 #After creating placeholders for the data, the graph is built from the mnist.py file according to a 3-stage pattern: inference(), loss(), and training().
 
-sess = tf.Session()
+# Store layers weight & bias
+weights = {
+    'wc1': tf.Variable(tf.random_normal([3, 3, 1, 64])),
+    'wc2': tf.Variable(tf.random_normal([3, 3, 64, 128])),
+    'wd1': tf.Variable(tf.random_normal([4*4*256, 1024])),
+    'wd2': tf.Variable(tf.random_normal([1024, 1024])),
+    'out': tf.Variable(tf.random_normal([1024, 8]))
+}
 
+biases = {
+    'bc1': tf.Variable(tf.random_normal([64])),
+    'bc2': tf.Variable(tf.random_normal([128])),
+    'bd1': tf.Variable(tf.random_normal([1024])),
+    'bd2': tf.Variable(tf.random_normal([1024])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
 
+# Construct model
+pred = alex_net(x, weights, biases, keep_prob)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# Evaluate model
+correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# Initializing the variables
+init = tf.initialize_all_variables()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    step = 1
+    # Keep training until reach max iterations
+    while step * batch_size < training_iters:
+        batch_xs, batch_ys = tf.train.batch(image_queue, batch_size)
+        # Fit training using batch data
+        sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys, keep_prob: dropout})
+        if step % display_step == 0:
+            # Calculate batch accuracy
+            acc = sess.run(accuracy, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 1.})
+            # Calculate batch loss
+            loss = sess.run(cost, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 1.})
+            print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + "{:.6f}".format(loss) + ", Training Accuracy= " + "{:.5f}".format(acc))
+        step += 1
+    print ("Optimization Finished!")
